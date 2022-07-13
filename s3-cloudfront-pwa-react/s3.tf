45f3421e4977b6d7f2fc12a5cf6ee07c2fd9f6eb
Cristiano Deucher <cristiano.deucher@objective.com.br>
2022-07-13 18:46:14 -0300

    fix: fixed s3 files upload

--- s3-cloudfront-pwa-react/s3.tf
+++ s3-cloudfront-pwa-react/s3.tf
@@ -45,24 +45,20 @@ resource "aws_s3_bucket_policy" "devops_app_bucket_policy" {
   policy = data.aws_iam_policy_document.devops_app_policy_document.json
 }
 
+resource "aws_s3_bucket_object" "current" {
+  bucket = aws_s3_bucket.devops_app_bucket.id
+  content_type = "application/x-directory"
+  key    = "current/"
+}
+
 resource "aws_s3_bucket_object" "static_sync" {
   for_each = toset([ for fn in fileset("${path.module}/src/s3_static", "**/*") : fn if length(regexall("^logotypes\\/r12n\\/[0-9]{14}\\.jpg$", fn)) == 0 ])
 
   bucket = aws_s3_bucket.devops_app_bucket.id
-  key    = "static/${each.value}"
+  key    = "current/${each.value}"
   source = "${path.module}/src/s3_static/${each.value}"
   etag   = filemd5("${path.module}/src/s3_static/${each.value}")
 
   content_type = "%{ if contains(["jpeg", ".jpg"], strrev(substr(strrev(each.value), 0, 4))) }image/jpeg%{ endif }%{ if contains(["png"], strrev(substr(strrev(each.value), 0, 3))) }image/png%{ endif }%{ if contains(["json"], strrev(substr(strrev(each.value), 0, 4))) }application/json%{ endif }%{ if contains(["apple-app-site-association"], strrev(substr(strrev(each.value), 0, 26))) }application/json%{ endif }%{ if contains(["html"], strrev(substr(strrev(each.value), 0, 4))) }text/html%{ endif }%{ if contains(["js"], strrev(substr(strrev(each.value), 0, 2))) }text/javascript%{ endif }%{ if contains(["css"], strrev(substr(strrev(each.value), 0, 3))) }text/css%{ endif }%{ if contains(["csv"], strrev(substr(strrev(each.value), 0, 3))) }text/csv%{ endif }"
 }
 
-resource "aws_s3_bucket_object" "current" {
-  bucket = aws_s3_bucket.devops_app_bucket.id
-  content_type = "application/x-directory"
-  key    = "current/"
-}
-resource "aws_s3_bucket_object" "next" {
-  bucket = aws_s3_bucket.devops_app_bucket.id
-  content_type = "application/x-directory"
-  key    = "next/"
-}
